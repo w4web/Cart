412b83c419768ec2af1e921fe561a3dfa5276ad4
w4web <w4websolution@gmail.com>
2022-03-03 13:19:15 +0530

    Category & product slider..

--- AngularCart/src/app/frontend/home/home.component.ts
+++ AngularCart/src/app/frontend/home/home.component.ts
@@ -21,6 +21,74 @@ export class HomeComponent implements OnInit {
     }
   ];
 
+  categories: any = [
+    {
+      img: "./assets/images/category1.jpg",
+      title: "Sports",
+      subTitle: "Get 10% Off Your Order"
+    },
+    {
+      img: "./assets/images/category2.jpg",
+      title: "Shoes",
+      subTitle: "Casual, Stylish & Trendy"
+    },
+    {
+      img: "./assets/images/category3.jpg",
+      title: "Watches",
+      subTitle: "Source for luxury watches"
+    },
+    {
+      img: "./assets/images/category4.jpg",
+      title: "Furniture",
+      subTitle: "Free Delivery in Town"
+    },
+    {
+      img: "./assets/images/category5.jpg",
+      title: "Glasses",
+      subTitle: "Fashion Glasses Frames"
+    }
+  ];
+
+  products: any = [
+    {
+      img: "./assets/images/product1.jpg",
+      title: "Oversized V Sweater",
+      price: "$45.00"
+    },
+    {
+      img: "./assets/images/product2.jpg",
+      title: "V-neck Blouse",
+      price: "$65.00"
+    },
+    {
+      img: "./assets/images/product3.jpg",
+      title: "Puffy Sleeves",
+      price: "$88.00"
+    },
+    {
+      img: "./assets/images/product4.jpg",
+      title: "Masculine Blazer",
+      price: "$65.00",
+      originalPrice: "$75.00"
+    },
+    {
+      img: "./assets/images/product5.jpg",
+      title: "High Rise Shorts",
+      price: "$55.00",
+      originalPrice: "$65.00"
+    },
+    {
+      img: "./assets/images/product1.jpg",
+      title: "Oversized V Sweater",
+      price: "$45.00"
+    },
+    {
+      img: "./assets/images/product2.jpg",
+      title: "V-neck Blouse",
+      price: "$65.00"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
