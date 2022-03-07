4c5052c15890d27db2227e052eca1269c9ea334d
w4web <w4websolution@gmail.com>
2022-03-07 17:13:36 +0530

    Product card & brand carowsel..

--- AngularCart/src/app/frontend/home/home.component.ts
+++ AngularCart/src/app/frontend/home/home.component.ts
@@ -89,6 +89,30 @@ export class HomeComponent implements OnInit {
     }
   ];
 
+  brands: any = [
+    {
+      img: "./assets/images/client-logos-1.png",
+    },
+    {
+      img: "./assets/images/client-logos-2.png",
+    },
+    {
+      img: "./assets/images/client-logos-3.png",
+    },
+    {
+      img: "./assets/images/client-logos-4.png",
+    },
+    {
+      img: "./assets/images/client-logos-5.png",
+    },
+    {
+      img: "./assets/images/client-logos-6.png",
+    },
+    {
+      img: "./assets/images/client-logos-7.png",
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
