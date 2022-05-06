f53dfaf35c6bd19aa0e089f46dff4e21c0e6e39f
w4web <w4websolution@gmail.com>
2022-05-06 17:03:52 +0530

    Cart page..

--- AngularCart/src/app/frontend/product/cart/cart.component.ts
+++ AngularCart/src/app/frontend/product/cart/cart.component.ts
@@ -11,6 +11,21 @@ export class CartComponent implements OnInit {
   breadcrumbItems!: MenuItem[];
   breadcrumbHome!: MenuItem;
 
+  products: any = [
+    {
+      img: "./assets/images/product1.jpg",
+      title: "Oversized V Sweater",
+      quantity: 1,
+      price: "$45.00"
+    },
+    {
+      img: "./assets/images/product2.jpg",
+      title: "V-neck Blouse",
+      quantity: 2,
+      price: "$65.00"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
