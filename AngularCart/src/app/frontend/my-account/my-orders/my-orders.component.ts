10a898d1f093b50ec7f36244647220bb91d9c95c
w4web <w4websolution@gmail.com>
2022-05-20 10:05:09 +0530

    Order page in progress..

--- AngularCart/src/app/frontend/my-account/my-orders/my-orders.component.ts
+++ AngularCart/src/app/frontend/my-account/my-orders/my-orders.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyOrdersComponent implements OnInit {
 
+  orders: any = [
+    {
+      
+    }
+  ]
+  products: any = [
+    {
+      img: "./assets/images/product1.jpg",
+      title: "Oversized V Sweater",
+      quantity: 1,
+      price: "$45.00"
+    },
+    {
+      img: "./assets/images/product2.jpg",
+      title: "V-neck Blouse",
+      quantity: 2,
+      price: "$65.00"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
