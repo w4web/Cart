61f16ea24c5c14db59b6c6a0141b81ef76bd6cdd
w4web <w4websolution@gmail.com>
2022-07-07 16:27:34 +0530

    Comments for un-Authenticated applications

--- API/routes/admin/product-route.js
+++ API/routes/admin/product-route.js
@@ -10,4 +10,12 @@ router.post('/add', isAuth, adminRole, productController.addProduct);
 router.put('/edit/:productId', isAuth, adminRole, productController.editProduct);
 router.post('/delete', isAuth, adminRole, productController.deleteProduct);
 
+// ***** Authentication removed for un-Authenticated applications *****
+
+// router.get('/', productController.getProducts);
+// router.get('/:productId', productController.getProduct);
+// router.post('/add', productController.addProduct);
+// router.put('/edit/:productId', productController.editProduct);
+// router.post('/delete', productController.deleteProduct);
+
 module.exports = router;
